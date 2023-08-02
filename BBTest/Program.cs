fced8b4147523f1aac7e4a326f960616dd8da6cf
j4587698 <jx@jvxiang.com>
2023-08-02 21:51:00 +0800

    Initial commit

---
+++ BBTest/Program.cs
@@ -0,0 +1,42 @@
+﻿using BBTest.Data;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+
+builder.Services.AddBootstrapBlazor();
+
+builder.Services.AddSingleton<WeatherForecastService>();
+
+// 增加 Table 数据服务操作类
+builder.Services.AddTableDemoDataService();
+// builder.Services.AddFreeSql((freeSqlBuilder) =>
+// {
+//     freeSqlBuilder
+//         .UseConnectionString(FreeSql.DataType.Sqlite, @"Data Source=|DataDirectory|\document.db; ")
+//         .UseAutoSyncStructure(true);
+// });
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+}
+
+app.UseStaticFiles();
+
+app.UseRouting();
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
